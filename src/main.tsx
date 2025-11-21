98e2e9249fe06d9210e9b3c02ba07aab84b93394
agustinlopez9 <fernandopiceda99@gmail.com>
2025-11-21 19:48:06 -0300

    fix for deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React, { StrictMode } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ChakraProvider, createSystem, defaultConfig } from "@chakra-ui/react";
 import App from "./App";
--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    allowedHosts: ["9f269a1e5f88.ngrok-free.app"],
-  },
   plugins: [react(), tsconfigPaths()],
-  base: "https://agustinlopez9.github.io/my-personal-portfolio/",
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          ui: ['@chakra-ui/react']
-        }
-      }
-    }
-  }
+  base: "/my-personal-portfolio",
 });
