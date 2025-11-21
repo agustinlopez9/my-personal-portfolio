9a5f82f885152851c02f64091a75e517fb81f63a
agustinlopez9 <fernandopiceda99@gmail.com>
2025-11-21 19:34:36 -0300

    change typescript and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,17 @@ export default defineConfig({
     allowedHosts: ["9f269a1e5f88.ngrok-free.app"],
   },
   plugins: [react(), tsconfigPaths()],
+  base: "https://agustinlopez9.github.io/my-personal-portfolio/",
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          ui: ['@chakra-ui/react']
+        }
+      }
+    }
+  }
 });
